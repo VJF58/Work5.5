0ecacd5de76ec85e91698e98301c933ef80a2b6d
Kuuna <sobakamail332@gmail.com>
2023-01-21 00:09:41 +0300

    EX1 comleted

--- Work5.5/Program.cs
+++ Work5.5/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    /// <summary>
+    /// Разбивает строку на слова
+    /// </summary>
+    /// <param name="text">Строка</param>
+    /// <returns>Массив слов</returns>
+    static string[] SplitText(string text)
+    {
+        string[] words = text.Split(' ');
+
+        return words;
+    }
+
+    /// <summary>
+    /// Выводит в консоль массив слов
+    /// </summary>
+    /// <param name="words">Массив слов</param>
+    static void ShowWords(string[] words)
+    {
+        foreach (string i in words)
+        {
+            Console.WriteLine(i);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите предложение: ");
+        string userString = Console.ReadLine();
+
+        ShowWords(SplitText(userString));
+
+        Console.ReadKey();
+    }
+}
