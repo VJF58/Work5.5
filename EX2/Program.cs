02c72cdb2ee2589e655c7986071a7ac83862aa9b
Kuuna <sobakamail332@gmail.com>
2023-01-21 21:37:31 +0300

    EX2 fixed

--- EX2/Program.cs
+++ EX2/Program.cs
@@ -1,33 +1,30 @@
-﻿internal class Program
+﻿using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
 {
-    /// <summary>
-    /// Записывает слова в строке в обратном порядке и выводит в консоль
-    /// </summary>
-    /// <param name="inputPhrase">Входная строка</param>
-    static void ReversWords(string inputPhrase)
+    static string ReversWords(string inputPhrase)
     {
-        string[] words = inputPhrase.Split(' ');
-        Console.WriteLine(Reverse(words));
+        string[] phrase = SplitText(inputPhrase);
+        string temp;
+
+
+        for (int i = 0; i < phrase.Length; i++)
+        {
+            if ((phrase.Length - 1 - i) <= i) break;
+
+            temp = phrase[i];
+            phrase[i] = phrase[phrase.Length - 1 - i];
+            phrase[phrase.Length - 1 - i] = temp; // Первый и последний элемент меняются значениями через временную переменную
+        }
+
+        return string.Join(' ', phrase);
     }
 
-    /// <summary>
-    /// Принимает массив строк на вход, записывает элементы в обратном порядке и возвращает эти элементы в виде строки
-    /// </summary>
-    /// <param name="text">Массив слов</param>
-    /// <returns></returns>
-    static string Reverse(string[] text)
+    static string[] SplitText(string text)
     {
-        string temp; //Переменная для временного хранения значений элементов массива
-        for (int i = 0; i < text.Length; i++)
-        {
-            if ((text.Length - 1 - i) <= i) break;
+        string[] words = text.Split(' ');
 
-            temp = text[i];
-            text[i] = text[text.Length - 1 - i];
-            text[text.Length - 1 - i] = temp; // Первый и последний элемент меняются значениями через временную переменную
-
-        }
-        return string.Join(' ', text);
+        return words;
     }
 
     private static void Main(string[] args)
@@ -38,6 +35,8 @@
         Console.WriteLine("Для продолжения нажмите enter");
         Console.ReadKey();
 
-        ReversWords(userString);
+        string reversedUserString = ReversWords(userString);
+
+        Console.WriteLine($"Строка с обратным порядком слов - {reversedUserString}");
     }
 }
